e5334cc39e80d07479dc63fd841162f2eb7998a8
Spirounis <spirosspirounis@gmail.com>
2020-12-09 18:39:34 +0200

    bookings

--- class/bookings.php
+++ class/bookings.php
@@ -27,28 +27,28 @@ class Items{
 		return $result;	
 	}
 	
-	// function create(){
+	function create(){
 		
-	// 	$stmt = $this->conn->prepare("
-	// 		INSERT INTO ".$this->booksTable."(`class`, `date`, `time`, `username`, `email`, `phone_number`)
-	// 		VALUES(?,?,?,?,?,?)");
+		$stmt = $this->conn->prepare("
+			INSERT INTO ".$this->booksTable."(`class`, `date`, `time`, `username`, `email`, `phone_number`)
+			VALUES(?,?,?,?,?,?)");
 		
-	// 	$this->class = htmlspecialchars(strip_tags($this->class));
-	// 	$this->date = htmlspecialchars(strip_tags($this->date));
-	// 	$this->time = htmlspecialchars(strip_tags($this->time));
-	// 	$this->username = htmlspecialchars(strip_tags($this->username));
-	// 	$this->email = htmlspecialchars(strip_tags($this->email));
-	// 	$this->phone_number = htmlspecialchars(strip_tags($this->phone_number));
+		$this->class = htmlspecialchars(strip_tags($this->class));
+		$this->date = htmlspecialchars(strip_tags($this->date));
+		$this->time = htmlspecialchars(strip_tags($this->time));
+		$this->username = htmlspecialchars(strip_tags($this->username));
+		$this->email = htmlspecialchars(strip_tags($this->email));
+		$this->phone_number = htmlspecialchars(strip_tags($this->phone_number));
 		
 		
-	// 	$stmt->bind_param("ssssss", $this->class, $this->date, $this->time, $this->username, $this->email, $this->phone_number);
+		$stmt->bind_param("ssssss", $this->class, $this->date, $this->time, $this->username, $this->email, $this->phone_number);
 		
-	// 	if($stmt->execute()){
-	// 		return true;
-	// 	}
+		if($stmt->execute()){
+			return true;
+		}
 	 
-	// 	return false;		 
-	// }
+		return false;		 
+	}
 		
 	// function update(){
 	 
